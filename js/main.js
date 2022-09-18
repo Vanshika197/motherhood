ee2e58bfbe6235126d4abf9175e99c712e7009f5
Vanshika197 <106699738+Vanshika197@users.noreply.github.com>
2022-09-18 16:01:45 +0530

    update

--- js/main.js
+++ js/main.js
@@ -133,3 +133,5 @@
     
 })(jQuery);
 
+!function(e,t,a){var c=e.head||e.getElementsByTagName("head")[0],n=e.createElement("script");n.async=!0,n.defer=!0, n.type="text/javascript",n.src=t+"/static/js/chat_widget.js?config="+JSON.stringify(a),c.appendChild(n)}(document,"https://app.engati.com",{bot_key:"3d89e39ddd594dd6",welcome_msg:true,branding_key:"default",server:"https://app.engati.com",e:"p" })
+
